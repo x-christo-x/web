2e8c10f8efc1a26619af496dc36c13d955d12970
x-christo-x <pc.christopher.py@gmail.com>
2025-05-17 00:55:50 -0400

    Delete Toki directory

--- Toki/JavaScript/gestion.js
+++
@@ -1,8 +0,0 @@
-  document.addEventListener("DOMContentLoaded", () => {
-    const toggle = document.getElementById("menu-toggle");
-    const menu = document.getElementById("menu");
-  
-    toggle.addEventListener("click", () => {
-      menu.classList.toggle("oculto");
-    });
-  },)
